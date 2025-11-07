fc983c14af47a634b209905b1c45a1c6138cb3b5
Adeel-Xtech <adeelxtech@gmail.com>
2025-11-07 21:12:29 +0500

    Update config.js

--- config.js
+++ config.js
@@ -79,6 +79,8 @@ DEV: process.env.DEV || "923035512967",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
 // make it true for auto recoding 
+ANTI_CALL: process.env.ANTI_CALL || "false"
+// make it true for auto call reject 
 };
